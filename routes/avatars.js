5315e110dc21ee07f1281af9c7b0d41e2841b270
cappuuuuuu <w1814946281@gmail.com>
2022-03-21 23:24:07 +0800

    fix: remove localhost test code

--- routes/avatars.js
+++ routes/avatars.js
@@ -3,10 +3,8 @@ const router = express.Router()
 const AvatarsModel = require('../models/avatars')
 
 router.get('/', async (req, res, next) => {
-  // const avatars = await AvatarsModel.find()
-  // const q = new Error('aaa')
-  // res.send(avatars)
-  next(new Error('XDDD'))
+  const avatars = await AvatarsModel.find()
+  res.send(avatars)
 })
 
 module.exports = router
