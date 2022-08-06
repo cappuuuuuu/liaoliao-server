33863a762d644b71dd972f71cdef067b4c56eb59
cappuuuuuu <w1814946281@gmail.com>
2022-08-06 13:58:00 +0800

    feat: add message delete API
    * Methods: DELETE
    * Request Parameter :  _id (type: Array)

--- routes/common/message.js
+++ routes/common/message.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const authToken = require('@middleware/authToken')
 const MessageModel = require('@models/message')
+const errorMessage = require('@static/error_message')
 
 router.get('/message', authToken, async (req, res) => {
   let { page, count } = req.query
@@ -15,7 +16,30 @@ router.get('/message', authToken, async (req, res) => {
     data: singlePageMessage,
     count: allMessage.length
   }
-  res.send(payload)
+  return res.json(payload)
+})
+
+router.delete('/message', authToken, async (req, res) => {
+  const { _id } = req.body
+  const isDelete = await MessageModel.deleteMany({ _id: { $in: _id } })
+
+  if (!isDelete) {
+    return res
+      .status(400)
+      .json({
+        status: 'error',
+        code: 404,
+        data: null,
+        message: errorMessage.MESSAGE_NOT_FOUND
+      })
+  }
+
+  const payload = {
+    status: 'success',
+    data: { deletedCount: isDelete.deletedCount },
+    message: null
+  }
+  return res.json(payload)
 })
 
 module.exports = router
