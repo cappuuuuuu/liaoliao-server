e5c0381bdf52482120415dd9e69568b4fe04ff5a
cappuuuuuu <w1814946281@gmail.com>
2022-08-18 00:55:41 +0800

    feat: add token authentication

--- routes/admin/logout.js
+++ routes/admin/logout.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 const operatorModel = require('@models/operator')
+const authToken = require('@middleware/authToken')
 const errorMessage = require('@static/error_message')
 const cookies = require('@static/cookies_key')
 const response = require('@utils/response')
 
-router.post('/logout', async (req, res) => {
+router.post('/logout', authToken, async (req, res) => {
   const operator = await operatorModel.findOne({ _id: req.body._id })
   if (!operator) {
     return res
